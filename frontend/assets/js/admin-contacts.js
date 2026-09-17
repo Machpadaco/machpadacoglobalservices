75a8f47c20fe6bbb92aa3ea0b191ecf963ca611e
EJIKE NJUWA <machpadacoglobalservices@gmail.com>
2026-09-17 16:22:05 +0000

    Sep 17, 2026, 5:22 PM

--- frontend/assets/js/admin-contacts.js
+++ frontend/assets/js/admin-contacts.js
@@ -15,7 +15,7 @@ const API_BASE_URL =
         window.location.hostname === "127.0.0.1"
     )
         ? "http://localhost:5000"
-        : "";
+        : "https://machpadacoglobalservices-api.onrender.com";
 
 
 // ========================================
