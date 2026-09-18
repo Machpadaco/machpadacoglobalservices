ce659d67f658e448c31be34846c28b5c2b1e5368
EJIKE NJUWA <machpadacoglobalservices@gmail.com>
2026-09-18 05:49:20 +0000

    Sep 18, 2026, 6:49 AM

---
+++ frontend/assets/js/config.js
@@ -0,0 +1,15 @@
+// ========================================
+// MACHpadaco Global Services
+// API Configuration
+// ========================================
+
+const API_BASE_URL =
+    (
+        window.location.hostname === "localhost" ||
+        window.location.hostname === "127.0.0.1"
+    )
+        ? "http://localhost:5000"
+        : "https://machpadacoglobalservices-api.onrender.com";
+
+
+export default API_BASE_URL;
